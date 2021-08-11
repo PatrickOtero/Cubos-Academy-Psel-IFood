8603995b5755e6445242dd4c145b163ff2de628f
Letícia Kira <patrick.rocha.otero@gmail.com>
2021-08-10 21:52:20 -0300

    A obra de 1 milhão de dólares

---
+++ Veri_o_valdo.js
@@ -0,0 +1,13 @@
+function solucao(obras) {
+	let obr = ""
+    let prec = 0
+    
+    for (let it of obras) {
+        if (it.valor > prec) {
+            prec = it.valor
+            obr = it.nome
+        }
+    }
+    console.log (obr)
+    
+}

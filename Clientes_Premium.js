65a5d2dbbd728d1adc60a1612d7d61816451f8b8
Letícia Kira <patrick.rocha.otero@gmail.com>
2021-08-10 21:28:40 -0300

    condicional do poder aquisitivo

---
+++ Clientes_Premium.js
@@ -0,0 +1,20 @@
+function solucao(precos) {
+    let soma = 0 
+    let vip = false
+        
+        for (let it of precos) {        
+            soma += it 
+            if (it > 200) {
+                vip = true
+            }
+           }
+    
+         if (soma > 1000 && vip) {
+          console.log ("PREMIUM")
+         } else if (soma > 1000 && !vip) {
+             console.log ("VIP")
+         } else {
+               console.log ("NORMAL")      
+           }
+
+}

271a6c80833c0c005cbb2964d0e045b098493185
Letícia Kira <patrick.rocha.otero@gmail.com>
2021-08-10 21:23:23 -0300

    Índice, imagem e array

---
+++ Carrossel.js
@@ -0,0 +1,23 @@
+function solucao(sequencia) {
+	let nav = sequencia.split("")
+    let val = 0
+    
+    for (let it of nav) {
+        if (it === ">") {
+            if (val < 6) {
+                val++
+            } else {
+                val = 0
+            }
+        } else if (it === "<") {
+            if (val > 0) {
+                val--
+            } else {
+                val = 6
+            }
+        }
+    }
+    
+    console.log (val)
+
+}

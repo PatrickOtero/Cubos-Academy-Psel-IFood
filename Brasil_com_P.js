c780cf64b7634ecd2e7d7b97f14c82c04640faf8
Letícia Kira <patrick.rocha.otero@gmail.com>
2021-08-10 21:21:09 -0300

    Mexendo com arrays e tipos

---
+++ Brasil_com_P.js
@@ -0,0 +1,14 @@
+function solucao(primeiraLetra, segundaLetra, palavras) {
+     
+    let nen = false
+    
+    for (let it of palavras){
+        if (it[0] === primeiraLetra && it[1] === segundaLetra) {
+           console.log(it)
+           nen = true    
+        } 
+    }
+       if (nen === false) {
+        console.log ("NENHUMA")
+    }
+}
